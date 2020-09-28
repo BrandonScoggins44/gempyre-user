f49d952b2ff13846073ac20e02d6888a822e4f32
Brandon Scoggins <brandon.scoggins44@gamil.com>
2020-09-28 15:07:23 -0400

    changes for heroku

--- src/main/java/com/gempyre/UserApplication.java
+++ src/main/java/com/gempyre/UserApplication.java
@@ -6,12 +6,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gempyre.model.User;
 import com.gempyre.repository.UserRepository;
 
+@Controller
 @SpringBootApplication
 public class UserApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
